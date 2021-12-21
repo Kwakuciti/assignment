2340e4601484cfaa1ac722614f612ba861074bcd
kwakuciti <kwekuciti@gmail.com>
2021-12-21 16:44:51 +0000

    mongoose

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { mongoDBConnection } from './config/db.js';
+import {UserRoute} from './route/userroute.js';
+
+
+mongoDBConnection();
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use('/users',UserRoute);
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
